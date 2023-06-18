ccc901e8c6041ef3aa5b60f5e13e578bd000f80b
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2023-06-18 10:03:10 +0100

    production

--- resort-be/config/environment.py
+++ resort-be/config/environment.py
@@ -1,2 +1,8 @@
+from os import environ
+from dotenv import load_dotenv
+
+ENVIRONMENT_FILE = environ.get('ENV_FILE') or '.env'
+load_dotenv(ENVIRONMENT_FILE, override=True)
+
 db_URI = "postgresql://localhost:5432/resorts_db"
 secret = 'correcthorsebatterystaple'
