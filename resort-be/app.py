8f623801bc27c77b986574b3c9dd4bf204967cc9
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2023-06-19 16:01:41 +0100

    production

--- resort-be/app.py
+++ resort-be/app.py
@@ -9,6 +9,7 @@ from config.environment import db_URI
 app = Flask(__name__)
 CORS(app)
 
+
 app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
