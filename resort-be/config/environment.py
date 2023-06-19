94bdc6eae41c6e31e8e0a765618ed383e54c243e
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2023-06-19 11:02:04 +0100

    production

--- resort-be/config/environment.py
+++ resort-be/config/environment.py
@@ -2,6 +2,7 @@ from os import environ
 from dotenv import load_dotenv
 
 ENVIRONMENT_FILE = environ.get('ENV_FILE') or '.env'
+
 load_dotenv(ENVIRONMENT_FILE, override=True)
 
 db_URI = environ.get('DB_URI')
