47edf7a45429a5cd4b0042969b28c9a90e660daf
Rob Wilhelmsson <rob.wilhelmsson@gmail.com>
2023-06-19 16:25:11 +0100

    cors issues

--- resort-be/app.py
+++ resort-be/app.py
@@ -16,6 +16,13 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"  # Replace * with your desired origin
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
 from controllers import resorts, users
 
 app.register_blueprint(resorts.router, url_prefix="/api")
